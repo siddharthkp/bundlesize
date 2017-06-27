b822ef7a080a8735cb7781ae6de0f246a6af0cd2
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2017-06-27 17:14:57 +0530

    move compare into reporter

--- index.js
+++ index.js
@@ -1,30 +1,6 @@
 #!/usr/bin/env node
 
-const bytes = require('bytes')
-const { error, info } = require('prettycli')
 const files = require('./src/files')
+const reporter = require('./src/reporter')
 
-const compare = files => {
-  let fail = false
-
-  files.map(file => {
-    const prettySize = bytes(file.size)
-    const prettyThreshold = bytes(bytes(file.threshold))
-
-    if (file.size <= bytes(file.threshold)) {
-      info(
-        `PASS`,
-        `${file.path}: ${prettySize} <= threshold ${prettyThreshold}`
-      )
-    } else {
-      fail = true
-      error(`${file.path}: ${prettySize} > threshold ${prettyThreshold}`, {
-        fail: false
-      })
-    }
-  })
-
-  if (fail) process.exit(1)
-}
-
-compare(files)
+reporter(files)
---
+++ src/reporter.js
@@ -0,0 +1,27 @@
+const bytes = require('bytes')
+const { error, info } = require('prettycli')
+
+const reporter = files => {
+  let fail = false
+
+  files.map(file => {
+    const prettySize = bytes(file.size)
+    const prettyThreshold = bytes(bytes(file.threshold))
+
+    if (file.size <= bytes(file.threshold)) {
+      info(
+        `PASS`,
+        `${file.path}: ${prettySize} <= threshold ${prettyThreshold}`
+      )
+    } else {
+      fail = true
+      error(`${file.path}: ${prettySize} > threshold ${prettyThreshold}`, {
+        fail: false
+      })
+    }
+  })
+
+  if (fail) process.exit(1)
+}
+
+module.exports = reporter
