6ecfe07d8bddb9130e9bf78cc181880dbf73861f
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2017-06-28 14:28:35 +0530

    error on unhandled promises

--- index.js
+++ index.js
@@ -2,5 +2,11 @@
 
 const files = require('./src/files')
 const reporter = require('./src/reporter')
+const build = require('./src/build')
 
 reporter(files)
+
+process.on('unhandledRejection', function(reason, p) {
+  console.log('Unhandled Promise: ', p, ' reason: ', reason)
+  build.error()
+})
