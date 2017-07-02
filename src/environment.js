f417cee43c653b74b9913e9adfb0edfdf5616928
Andreas Hoffmann <furizaa@gmail.com>
2017-07-02 16:27:27 +0200

    UnitTests: Add environemnt-test

--- src/environment.js
+++ src/environment.js
@@ -3,22 +3,26 @@ module.exports = getEnv => {
     getEnv = variable => process.env[variable];
   }
 
+  const token = getEnv('github_token') || getEnv('GITHUB_TOKEN');
+
   // See https://circleci.com/docs/1.0/environment-variables/
-  if (getEnv('CIRCLECI') && getEnv('github_token')) {
+  if (
+    getEnv('CIRCLECI') && token
+  ) {
     return {
       repo: `${getEnv('CIRCLE_PROJECT_USERNAME')}/${getEnv(
         'CIRCLE_PROJECT_REPONAME'
       )}`,
-      token: getEnv('github_token') || getEnv('GITHUB_TOKEN'),
+      token: token,
       event_type: getEnv('CIRCLE_PULL_REQUEST') ? 'pull_request' : 'push',
       sha: getEnv('CIRCLE_SHA1'),
       branch: getEnv('CIRCLE_BRANCH'),
     };
-  } else if (getEnv('TRAVIS') && getEnv('github_token')) {
+  } else if (getEnv('TRAVIS') && token) {
     // See https://docs.travis-ci.com/user/environment-variables/.
     return {
       repo: getEnv('TRAVIS_REPO_SLUG'),
-      token: getEnv('github_token') || getEnv('GITHUB_TOKEN'),
+      token: token,
       event_type: getEnv('TRAVIS_EVENT_TYPE'),
       sha: getEnv('TRAVIS_PULL_REQUEST_SHA') || getEnv('TRAVIS_COMMIT'),
       branch:
