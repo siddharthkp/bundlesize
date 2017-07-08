3499d8fce9d66e84ee97e32802a6f025a44ac057
Simon Legg <leggsimon@gmail.com>
2017-07-08 22:01:31 +0100

    Adds basic CLI interface.

--- index.js
+++ index.js
@@ -3,8 +3,34 @@
 const files = require('./src/files')
 const reporter = require('./src/reporter')
 const build = require('./src/build')
+const packageJsonConfig = require('./src/config');
+const { error } = require('prettycli')
+const program = require('commander')
 
-reporter(files)
+const currentVersion = require('./package.json').version
+const DEFAULT_SIZE = '3 kb';
+
+program
+  .version(currentVersion)
+  .option('-f, --files [files]', 'Files to test against e.g. dist/*.js (String)')
+  .option('-s, --max-size [maxSize]', 'Maximum size threshold e.g. 3Kb (String)', '3 kb')
+  .parse(process.argv);
+
+
+let cliConfig;
+
+if (program.files && program.maxSize) {
+  cliConfig = [{
+    path: program.files,
+    maxSize: program.maxSize
+  }]
+}
+
+if (!packageJsonConfig && !cliConfig) error('Config not found', { silent: true })
+
+const results = files(packageJsonConfig || cliConfig);
+
+reporter(results)
 
 process.on('unhandledRejection', function(reason, p) {
   console.log('Unhandled Promise: ', p, ' reason: ', reason)
