984e60270b73fb8b33c2d1602895d4dcecbecd31
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2017-07-16 01:07:42 +0530

    encode url

--- src/reporter.js
+++ src/reporter.js
@@ -49,7 +49,9 @@ const compare = (files, masterValues = {}) => {
     debug('message', message)
   })
 
-  const params = JSON.stringify({files, commit_message, branch})
+  const params = encodeURIComponent(
+    JSON.stringify({ files, commit_message, branch })
+  )
   const url = `https://bundlesize-store.now.sh/build?info=${params}`
 
   debug('url', url)
