f3ace71ae409f14145f3fe3026f906951d61f777
Sara Vieira <hey@iamsaravieira.com>
2017-08-26 21:29:50 +0100

    show folder in error message

--- src/files.js
+++ src/files.js
@@ -1,26 +1,28 @@
-const fs = require('fs')
-const bytes = require('bytes')
-const glob = require('glob')
-const gzip = require('gzip-size')
-const { error } = require('prettycli')
-const config = require('./config')
-const debug = require('./debug')
+const fs = require('fs');
+const bytes = require('bytes');
+const glob = require('glob');
+const gzip = require('gzip-size');
+const { error } = require('prettycli');
+const config = require('./config');
+const debug = require('./debug');
 
-const files = []
+const files = [];
 
 config.map(file => {
-  const paths = glob.sync(file.path)
+  const paths = glob.sync(file.path);
   if (!paths.length) {
-    error('There is no matching file for ' + file.path, { silent: true })
+    error(`There is no matching file for ${file.path} in ${process.cwd()}`, {
+      silent: true
+    })
   } else {
     paths.map(path => {
-      const size = gzip.sync(fs.readFileSync(path, 'utf8'))
-      const maxSize = bytes(file.threshold || file.maxSize) || Infinity
-      files.push({ maxSize, path, size })
-    })
+      const size = gzip.sync(fs.readFileSync(path, 'utf8'));
+      const maxSize = bytes(file.threshold || file.maxSize) || Infinity;
+      files.push({ maxSize, path, size });
+    });
   }
-})
+});
 
-debug('files', files)
+debug('files', files);
 
-module.exports = files
+module.exports = files;
