71251017abfe96eddc8cb138599787583d5ef885
Sara Vieira <hey@iamsaravieira.com>
2017-08-27 12:27:21 +0100

    try again

--- dangerfile.js
+++ dangerfile.js
@@ -1,9 +1,12 @@
-import { danger, warn } from 'danger';
+import { danger, fail, warn } from 'danger'
 
 // Thanks to https://github.com/styleguidist/react-styleguidist/blob/master/dangerfile.js and sapegin
-const packageChanged = danger.git.modified_files.includes('package.json');
-const lockfileChanged = danger.git.modified_files.includes('package-lock.json');
+const packageChanged = danger.git.modified_files.includes('package.json')
+const lockfileChanged = danger.git.modified_files.includes('package-lock.json')
 if (packageChanged && !lockfileChanged) {
-  warn(`Changes were made to package.json, but not to package-lock.json.
-Perhaps you need to run \`rm package-lock.json && npm install\`. Make sure you’re using npm 5+.`);
+  fail(`Changes were made to package.json, but not to package-lock.json.
+Perhaps you need to run \`rm package-lock.json && npm install\`. Make sure you’re using npm 5+.`)
+}
+if (danger.github.pr.body.length < 10) {
+  warn('This pull request needs an description.')
 }
