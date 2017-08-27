122fa1b9805efb81ac4c2f36e92d6ede9b32fa00
Josh Hawkins <hawkins@users.noreply.github.com>
2017-08-26 17:34:56 -0700

    Add prettier pre-commit hook (#105)
    * Add prettier as pre-commit hook
    
    * Run prettier on codebase
    
    * Move husky, lint-staged to devDeps

--- src/reporter.js
+++ src/reporter.js
@@ -51,7 +51,9 @@ const compare = (files, masterValues = {}) => {
   })
 
   /* prepare the build page */
-  const params = encodeURIComponent(JSON.stringify({ files, repo, branch, commit_message, sha }))
+  const params = encodeURIComponent(
+    JSON.stringify({ files, repo, branch, commit_message, sha })
+  )
   let url = `https://bundlesize-store.now.sh/build?info=${params}`
 
   debug('url before shortening', url)
