848ae0396724767134d0ac41a565375679420156
Sara Vieira <hey@iamsaravieira.com>
2017-08-28 21:06:26 +0100

    revert to es5

--- dangerfile.js
+++ dangerfile.js
@@ -1,8 +1,9 @@
 const { danger, fail, warn } = require('danger')
 
 // Thanks to https://github.com/styleguidist/react-styleguidist/blob/master/dangerfile.js and sapegin
-const packageChanged = danger.git.modified_files.includes('package.json')
-const lockfileChanged = danger.git.modified_files.includes('package-lock.json')
+const packageChanged = danger.git.modified_files.indexOf('package.json') !== -1
+const lockfileChanged =
+  danger.git.modified_files.indexOf('package-lock.json') !== -1
 if (packageChanged && !lockfileChanged) {
   fail(`Changes were made to package.json, but not to package-lock.json.
 Perhaps you need to run \`rm package-lock.json && npm install\`. Make sure you’re using npm 5+.`)
