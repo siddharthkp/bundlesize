ca6ca7cf24d08fa8243c3b874225f3e1121da6be
Sara Vieira <hey@iamsaravieira.com>
2017-08-28 21:23:33 +0100

    update danger

--- dangerfile.js
+++ dangerfile.js
@@ -1,17 +1,14 @@
 const { danger, fail, warn } = require('danger')
 
 // Thanks to https://github.com/styleguidist/react-styleguidist/blob/master/dangerfile.js and sapegin
-// const packageChanged = danger.git.modified_files.indexOf('package.json') !== -1;
-// const lockfileChanged =
-//   danger.git.modified_files.indexOf('package-lock.json') !== -1;
-// if (packageChanged && !lockfileChanged) {
-//   fail(`Changes were made to package.json, but not to package-lock.json.
-// Perhaps you need to run \`rm package-lock.json && npm install\`. Make sure you’re using npm 5+.`);
-// }
+const packageChanged = danger.git.modified_files.indexOf('package.json') !== -1
+const lockfileChanged =
+  danger.git.modified_files.indexOf('package-lock.json') !== -1
+if (packageChanged && !lockfileChanged) {
+  fail(`Changes were made to package.json, but not to package-lock.json.
+Perhaps you need to run \`rm package-lock.json && npm install\`. Make sure you’re using npm 5+.`)
+}
 
-warn(danger.git.modified_files)
-warn(danger.github.pr)
-
-// if (danger.github.pr.body.length < 10) {
-//   warn('This pull request needs an description.');
-// }
+if (danger.github.pr.body.length < 10) {
+  warn('This pull request needs an description.')
+}
