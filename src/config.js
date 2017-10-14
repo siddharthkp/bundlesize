c766e711dde4e8724214d24b9dcdb22cc54300d1
erichdev <a.erich@gmail.com>
2017-10-13 19:07:49 -0700

    Fix auto-formatting applied by text editor

--- src/config.js
+++ src/config.js
@@ -13,10 +13,7 @@ const packageJSONconfig = pkg.bundlesize
 program
   .option('-f, --files [files]', 'files to test against (dist/*.js)')
   .option('-s, --max-size [maxSize]', 'maximum size threshold (3Kb)')
-  .option(
-    '-n, --display-name [displayName]',
-    'filename to display on Github UI'
-  )
+  .option('-n, --display-name [displayName]', 'filename to display on Github UI')
   .option('--debug', 'run in debug mode')
   .parse(process.argv)
 
