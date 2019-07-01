e7af916449cf4fbb452bc6661d3fbae401cba2fb
siddharthkp <siddharth.kshetrapal@gmail.com>
2019-07-01 17:35:08 +0530

    more tests

--- tests/index.js
+++ tests/index.js
@@ -3,11 +3,13 @@ const { commandSync: cmd } = require('execa')
 
 const bundlesize = `node ../../../index`
 
-function run(fixture) {
+function run(fixture, customParams = '') {
   let output
 
   try {
-    output = cmd(bundlesize, { cwd: `tests/fixtures/${fixture}` })
+    output = cmd(`${bundlesize} ${customParams}`, {
+      cwd: `tests/fixtures/${fixture}`
+    })
   } catch (error) {
     output = error
   }
@@ -27,5 +29,15 @@ test.serial('fail: single file larger than limit', t => {
 
 test.serial('pass: use brotli which should pass', t => {
   const { stdout, exitCode } = run(3)
+  t.snapshot({ stdout, exitCode })
+})
+
+test.serial('fail: use no compression which should fail', t => {
+  const { stdout, exitCode } = run(4)
+  t.snapshot({ stdout, exitCode })
+})
+
+test.serial('pass: custom config file', t => {
+  const { stdout, exitCode } = run(5, '--config config/bundlesize.json')
   t.snapshot({ stdout, exitCode })
 })
