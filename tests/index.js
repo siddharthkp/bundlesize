65b7f77ef52be693bf6c1283340618a46beb86c3
siddharthkp <siddharth.kshetrapal@gmail.com>
2019-07-01 15:17:41 +0530

    baby's first test

---
+++ tests/index.js
@@ -0,0 +1,26 @@
+const test = require('ava')
+const { commandSync: cmd } = require('execa')
+
+const bundlesize = `node ../../../index`
+
+function run(fixture) {
+  let output
+
+  try {
+    output = cmd(`${bundlesize}`, { cwd: `tests/fixtures/${fixture}` })
+  } catch (error) {
+    output = error
+  }
+
+  return output
+}
+
+test.serial('pass: single file smaller than limit', t => {
+  const { stdout, exitCode } = run(1)
+  t.snapshot({ stdout, exitCode })
+})
+
+test.serial('fail: single file larger than limit', t => {
+  const { stdout, exitCode } = run(2)
+  t.snapshot({ stdout, exitCode })
+})
