99a54d989dabba6f9616eefe5f68b705deccfdac
Mohsin Ul Haq <mohsinulhaq01@gmail.com>
2019-10-07 02:09:36 +0530

    fix firebase get method not returning its promise

--- store/firebase.js
+++ store/firebase.js
@@ -31,17 +31,19 @@ const set = (repo, values, sha, token) => {
 const get = (repo, token) => {
   authenticate(token)
   const ref = `${token}/${repo}`
-  database
+  return database
     .ref(ref)
     .limitToLast(1)
     .once('value')
     .then(snapshot => {
+      logout()
       const object = snapshot.val()
       if (!object) return []
-      const values = Object.values(object)[0]
-      return values
+      return Object.values(object)[0]
     })
-  logout()
+    .catch(() => {
+      logout()
+    })
 }
 
 module.exports = { set, get }
