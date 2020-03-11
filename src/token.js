e4e829bc460a6b9c43169c1f4cfb581f3d862baa
Alexander Bell-Towne <code@bell-towne.com>
2020-03-11 13:58:07 -0400

    Remove GITHUB_TOKEN from token.js
    This was causing an error with generic CI tokens, this should only use a project specific token, closes #93

--- src/token.js
+++ src/token.js
@@ -1,7 +1,5 @@
 const token =
   process.env.bundlesize_github_token ||
-  process.env.BUNDLESIZE_GITHUB_TOKEN ||
-  process.env.github_token ||
-  process.env.GITHUB_TOKEN
+  process.env.BUNDLESIZE_GITHUB_TOKEN
 
 module.exports = token
